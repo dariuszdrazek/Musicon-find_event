0e20b496b88e8053ad2f7b192a4538ee29fe7868
Dariusz Drazek <dariusz.drazek88@gmail.com>
2019-08-18 12:34:37 +0200

    basic structure + footer

---
+++ src/components/+Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+     return (
+          <footer>
+               <p>&copy; Dariusz Drążek wszystkie prawa zastrzeżone</p>
+          </footer>
+     );
+};
+
+export default Footer;
---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Footer from './+Footer';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
+import '../scss/main.scss';
+const App = () => {
+     return (
+          <div>
+               <Footer />
+          </div>
+     );
+};
+
+export default App;
